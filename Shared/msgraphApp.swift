fea04ee69c1047d76757a747d70f7639f2252477
øllen <30601311+oliverwk@users.noreply.github.com>
2021-05-25 14:12:05 +0200

    Initial Commit

---
+++ Shared/msgraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  msgraphApp.swift
+//  Shared
+//
+//  Created by Maarten Wittop Koning on 25/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct msgraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
